201603d2783592221bce6f95eda5e21add777c2c
Carson Bush <hyperdriveguy@gmail.com>
2024-07-12 10:52:46 -0600

    port conversation history controller

---
+++ maeser/controllers/conversation_history.py
@@ -0,0 +1,18 @@
+from flask import jsonify, request
+from common.render import get_response_html
+
+def conversation_history_controller(session_handler):
+    data = request.get_json()
+
+    session = data.get('session')
+    branch = data.get('branch')
+    
+    conversation_history = session_handler.get_conversation_history(branch, session)
+    if 'messages' in conversation_history:
+        for message in conversation_history['messages']:
+            if message['role'] == 'system':
+                message['content'] = get_response_html(message['content'])
+            else:
+                message['content'] = message['content']
+
+    return jsonify(conversation_history)
