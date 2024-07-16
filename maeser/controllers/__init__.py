8eaa2200755ab4ad0e51dab44d02eba1fbe00b23
Carson Bush <hyperdriveguy@gmail.com>
2024-07-16 08:49:49 -0600

    Better controller names Distinguish between logs overview and individual log view

--- maeser/controllers/__init__.py
+++ maeser/controllers/__init__.py
@@ -1,9 +1,12 @@
+from flask import Blueprint, abort
+from jinja2 import TemplateNotFound
+
 from . import (
     chat_interface,
-    chat_logs,
-    chat_tests,
-    display_log,
-    display_test,
+    chat_logs_overview,
+    chat_tests_overview,
+    display_chat_log,
+    display_chat_test,
     feedback_form,
     general_feedback,
     login,
@@ -18,10 +21,10 @@ from . import common
 
 __all__ = [
     "chat_interface",
-    "chat_logs",
-    "chat_tests",
-    "display_log",
-    "display_test",
+    "chat_logs_overview",
+    "chat_tests_overview",
+    "display_chat_log",
+    "display_chat_test",
     "feedback_form",
     "general_feedback",
     "login",
@@ -35,3 +38,5 @@ __all__ = [
     "remaining_requests_api",
     "common",
 ]
+
+
