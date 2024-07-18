87336aeeefc055c21775be71be3fe0fb4a06937e
Blaine Freestone <blainefreestone01@gmail.com>
2024-07-18 10:09:34 -0600

    removed all import

--- maeser/controllers/__init__.py
+++ maeser/controllers/__init__.py
@@ -24,26 +24,6 @@ from . import (
 from . import chat_api, conversation_history_api, feedback_api, remaining_requests_api
 from . import common
 
-__all__ = [
-    "chat_interface",
-    "chat_logs_overview",
-    "chat_tests_overview",
-    "display_chat_log",
-    "display_chat_test",
-    "feedback_form_get",
-    "save_feedback_form",
-    "login",
-    "logout",
-    "new_session_api",
-    "training",
-    "training_post",
-    "chat_api",
-    "conversation_history_api",
-    "feedback_api",
-    "remaining_requests_api",
-    "common",
-]
-
 # Get the directory of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__ + '/..'))
 
