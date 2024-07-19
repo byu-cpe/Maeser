b4f42dfce1273c084b3a5c0d1a7d3c689d7bf8a0
Blaine Freestone <blainefreestone01@gmail.com>
2024-07-19 16:40:57 -0600

    creates vectorstore from text extracted and saves it locally

--- example/create_vectorstore.py
+++ example/create_vectorstore.py
@@ -8,3 +8,14 @@ wiki_wiki = wikipediaapi.Wikipedia(
 
 p_wiki = wiki_wiki.page("Karl G. Maeser")
 text = p_wiki.text
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+documents = text_splitter.create_documents([text])
+
+from langchain_community.vectorstores import FAISS
+
+db = FAISS.from_documents(documents, OpenAIEmbeddings())
+db.save_local("example/vectorstores/maeser")
