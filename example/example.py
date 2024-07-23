3db811c1bb92cc7e1c94b53f8f54d040a52c12f3
Blaine Freestone <blainefreestone01@gmail.com>
2024-07-23 09:03:41 -0600

    updated example documentatino

--- example/example.py
+++ example/example.py
@@ -1,10 +1,5 @@
-from langgraph.graph.graph import CompiledGraph
-from maeser.user_manager import UserManager, GithubAuthenticator
 from maeser.chat.chat_logs import ChatLogsManager
 from maeser.chat.chat_session_manager import ChatSessionManager
-from maeser.blueprints import add_flask_blueprint
-from maeser.graphs.simple_rag import get_simple_rag
-from flask import Flask
 
 chat_logs_manager = ChatLogsManager("chat_logs")
 sessions_manager = ChatSessionManager(chat_logs_manager=chat_logs_manager)
@@ -23,17 +18,27 @@ byu_prompt: str = """You are speaking about the history of Brigham Young Univers
     {context}
     """
 
+from maeser.graphs.simple_rag import get_simple_rag
+from langgraph.graph.graph import CompiledGraph
+
 maeser_simple_rag: CompiledGraph = get_simple_rag("example/vectorstores/maeser", "index", "chat_logs/maeser.db", system_prompt_text=maeser_prompt)
 sessions_manager.register_branch("maeser", "Karl G. Maeser History", maeser_simple_rag)
 
 byu_simple_rag: CompiledGraph = get_simple_rag("example/vectorstores/byu", "index", "chat_logs/byu.db", system_prompt_text=byu_prompt)
 sessions_manager.register_branch("byu", "BYU History", byu_simple_rag)
 
+from maeser.user_manager import UserManager, GithubAuthenticator
+
 github_authenticator = GithubAuthenticator("...", "...", "http://localhost:5000/login/github_callback")
 user_manager = UserManager("chat_logs/users", max_requests=5, rate_limit_interval=60)
 user_manager.register_authenticator("github", github_authenticator)
 
+from flask import Flask
+
 base_app = Flask(__name__)
+
+from maeser.blueprints import add_flask_blueprint
+
 app: Flask = add_flask_blueprint(
     base_app, 
     "secret",
