b7a74ee0e818353424dc2d25f8d6c8b35d96979f
Carson Bush <hyperdriveguy@gmail.com>
2024-07-30 10:33:11 -0600

    Add license header to previously documented controllers

--- maeser/controllers/chat_interface.py
+++ maeser/controllers/chat_interface.py
@@ -1,6 +1,21 @@
 """Module for handling chat interface rendering.
 
 This module contains a function to render the chat interface template with relevant data.
+
+© 2024 Blaine Freestone, Carson Bush
+
+This file is part of Maeser.
+
+Maeser is free software: you can redistribute it and/or modify it under the terms of
+the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Maeser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+Maeser. If not, see <https://www.gnu.org/licenses/>.
 """
 
 from flask import render_template
