43564969a57dc6bd40621727830d9d1b2b8d67cf
Carson Bush <hyperdriveguy@gmail.com>
2024-07-30 10:17:44 -0600

    Add docstring and license header to controller subpackage init

--- maeser/controllers/__init__.py
+++ maeser/controllers/__init__.py
@@ -1,3 +1,22 @@
+"""
+This subpackage contains the logic for all built-in endpoints, including web pages and APIs.
+
+© 2024 Carson Bush, Blaine Freestone
+
+This file is part of Maeser.
+
+Maeser is free software: you can redistribute it and/or modify it under the terms of
+the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Maeser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+Maeser. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from . import (
     chat_api,
     chat_interface,
@@ -16,18 +35,18 @@ from . import (
 from . import common
 
 __all__ = [
-    "chat_api",
-    "chat_interface",
-    "chat_logs_overview",
-    "display_chat_log",
-    "feedback_api",
-    "feedback_form_get",
-    "feedback_form_post",
-    "login_api",
-    "logout",
-    "new_session_api",
-    "training",
-    "training_post",
-    "conversation_history_api",
-    "common",
+    'chat_api',
+    'chat_interface',
+    'chat_logs_overview',
+    'display_chat_log',
+    'feedback_api',
+    'feedback_form_get',
+    'feedback_form_post',
+    'login_api',
+    'logout',
+    'new_session_api',
+    'training',
+    'training_post',
+    'conversation_history_api',
+    'common',
 ]
