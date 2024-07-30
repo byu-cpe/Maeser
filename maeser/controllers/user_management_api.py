f089c10e65744f4bc93d52ac889a6aa9e9156d2a
Carson Bush <hyperdriveguy@gmail.com>
2024-07-30 10:44:23 -0600

    Add docstrings for user management web view

--- maeser/controllers/user_management_api.py
+++ maeser/controllers/user_management_api.py
@@ -4,6 +4,21 @@ User management API controller.
 This module provides an API endpoint for managing users, including listing users,
 toggling admin and banned statuses, updating request counts, removing users from
 the cache, fetching users, listing cleanable users, and cleaning the cache.
+
+© 2024 Carson Bush
+
+This file is part of Maeser.
+
+Maeser is free software: you can redistribute it and/or modify it under the terms of
+the GNU Lesser General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Maeser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+Maeser. If not, see <https://www.gnu.org/licenses/>.
 """
 
 from typing import Any
