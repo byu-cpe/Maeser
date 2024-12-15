d74e0dc8d9cd791c8d599804ac61d13911d50a87
Gohaun Manley <gohaun@byu.edu>
2024-12-14 23:35:01 -0700

    Added Config files from Verity
    Added Config files to the example code, a feature from the Verity project ported over here.
    Fixed bug in previous code where i imported a non existent class

---
+++ example/config_example.py
@@ -0,0 +1,38 @@
+"""
+Module containing yaml config loading.
+"""
+import yaml
+import os
+
+def load_config():
+    """Load configuration from YAML file."""
+    # This will load config from the first valid path. /etc/verity.conf has precedence above directories.
+    config_paths = [
+        'config_example.yaml',  # Current directory
+        './config_example.yaml',  # Parent directory (assuming script is run directly in src
+        'example/config_example.yaml'
+    ]
+    
+    for path in config_paths:
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                print(f'Using configuration at {path} (Priority {config_paths.index(path)})')
+                return yaml.safe_load(file)
+    
+    print("Warning: No configuration file found")
+    return {}
+
+config = load_config()
+MAX_REQUESTS_REMAINING = int(config.get('rate_limit', {}).get('max_requests_remaining'))
+TEST_YAML_OUTPUT_PATH = config.get('application', {}).get('test_yaml_output_path')
+VEC_STORE_TYPE = config.get('vectorstore', {}).get('vec_store_type', "faiss")
+VEC_STORE_PATH = config.get('vectorstore', {}).get('vec_store_path')
+LLM_MODEL_NAME = config.get('llm', {}).get('llm_model_name', "gpt4o")
+RATE_LIMIT_INTERVAL = int(config.get('rate_limit', {}).get('rate_limit_interval_seconds'))
+USERS_DB_PATH = config.get('user_management', {}).get('accounts_db_path')
+OPENAI_API_KEY = config.get('api_keys', {}).get('openai_api_key')
+GITHUB_CLIENT_ID = config.get('application', {}).get('github_client_id')
+GITHUB_CLIENT_SECRET = config.get('api_keys', {}).get('github_client_secret')
+GITHUB_AUTH_CALLBACK_URI = config.get('application', {}).get('github_auth_callback_uri')
+LOG_SOURCE_PATH = config.get('logging', {}).get('log_source_path')
+CHAT_HISTORY_PATH = config.get('user_management', {}).get('chat_history_path')
--- example/flask_example_user_mangement.py
+++ example/flask_example_user_mangement.py
@@ -17,8 +17,17 @@ Maeser. If not, see <https://www.gnu.org/licenses/>.
 
 from maeser.chat.chat_logs import ChatLogsManager
 from maeser.chat.chat_session_manager import ChatSessionManager
+from config_example import (
+    LOG_SOURCE_PATH, OPENAI_API_KEY, 
+    USERS_DB_PATH, VEC_STORE_PATH, MAX_REQUESTS_REMAINING, 
+    RATE_LIMIT_INTERVAL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET,
+    GITHUB_AUTH_CALLBACK_URI, CHAT_HISTORY_PATH
+)
+import os
 
-chat_logs_manager = ChatLogsManager("chat_logs")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+chat_logs_manager = ChatLogsManager(CHAT_HISTORY_PATH)
 sessions_manager = ChatSessionManager(chat_logs_manager=chat_logs_manager)
 
 maeser_prompt: str = """You are speaking from the perspective of Karl G. Maeser.
@@ -38,16 +47,20 @@ byu_prompt: str = """You are speaking about the history of Brigham Young Univers
 from maeser.graphs.simple_rag import get_simple_rag
 from langgraph.graph.graph import CompiledGraph
 
-maeser_simple_rag: CompiledGraph = get_simple_rag(vectorstore_path="vectorstores/maeser", vectorstore_index="index", memory_filepath="chat_logs/maeser.db", system_prompt_text=maeser_prompt)
+maeser_simple_rag: CompiledGraph = get_simple_rag(vectorstore_path=f"{VEC_STORE_PATH}/maeser", vectorstore_index="index", memory_filepath=f"{LOG_SOURCE_PATH}/maeser.db", system_prompt_text=maeser_prompt)
 sessions_manager.register_branch(branch_name="maeser", branch_label="Karl G. Maeser History", graph=maeser_simple_rag)
 
-byu_simple_rag: CompiledGraph = get_simple_rag(vectorstore_path="vectorstores/byu", vectorstore_index="index", memory_filepath="chat_logs/byu.db", system_prompt_text=byu_prompt)
+byu_simple_rag: CompiledGraph = get_simple_rag(vectorstore_path=f"{VEC_STORE_PATH}/byu", vectorstore_index="index", memory_filepath=f"{LOG_SOURCE_PATH}/byu.db", system_prompt_text=byu_prompt)
 sessions_manager.register_branch(branch_name="byu", branch_label="BYU History", graph=byu_simple_rag)
 
-from maeser.user_manager import UserManager, GithubAuthenticator, CAEDMAuthenticator, LDAPAuthenticator
+from maeser.user_manager import UserManager, GithubAuthenticator, LDAPAuthenticator
 
 # Replace the '...' with a client id and secret from a GitHub OAuth App that you generate
-github_authenticator = GithubAuthenticator(client_id="...", client_secret="...", auth_callback_uri="http://localhost:3000/login/github_callback")
+github_authenticator = GithubAuthenticator(
+    client_id=GITHUB_CLIENT_ID, 
+    client_secret=GITHUB_CLIENT_SECRET, 
+    auth_callback_uri=GITHUB_AUTH_CALLBACK_URI
+)
 caedm_authenticator = LDAPAuthenticator(
     name='CAEDM',
     ldap_server_urls=['ctldap.et.byu.edu', 'cbldap.et.byu.edu'],
@@ -59,7 +72,7 @@ caedm_authenticator = LDAPAuthenticator(
     ca_cert_path='/etc/ssl/certs',
     connection_timeout=5
 )
-user_manager = UserManager(db_file_path="chat_logs/users", max_requests=5, rate_limit_interval=60)
+user_manager = UserManager(db_file_path=USERS_DB_PATH, max_requests=MAX_REQUESTS_REMAINING, rate_limit_interval=RATE_LIMIT_INTERVAL)
 user_manager.register_authenticator(name="github", authenticator=github_authenticator)
 user_manager.register_authenticator(name="ldap", authenticator=caedm_authenticator)
 
@@ -82,5 +95,5 @@ app: Flask = add_flask_blueprint(
 )
 
 if __name__ == "__main__":
-    extra_files = ["maeser/data/templates/chat_interface.html", "maeser/data/templates/login.html", "/home/gohaun/Projects/maeser/maeser/data/static/styles.css"]
+    extra_files = ["maeser/data/templates/chat_interface.html", "maeser/data/templates/login.html", "maeser/data/static/styles.css"]
     app.run(port=3002, debug=True, extra_files=extra_files)
