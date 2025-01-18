0084d57d80a4fbaa853b58af47d47e1defcd3d4f
Gohaun Manley <gohaun@byu.edu>
2025-01-17 17:10:53 -0700

    Added "..." for all secrets in config_example.yaml
    currently working on getting flask example without user manager to work

--- example/flask_example_user_mangement.py
+++ example/flask_example_user_mangement.py
@@ -55,7 +55,7 @@ sessions_manager.register_branch(branch_name="byu", branch_label="BYU History", 
 
 from maeser.user_manager import UserManager, GithubAuthenticator, LDAPAuthenticator
 
-# Replace the '...' with a client id and secret from a GitHub OAuth App that you generate
+# Replace the '...' with a client id and secret from a GitHub OAuth App that you generate from config_example.yaml
 github_authenticator = GithubAuthenticator(
     client_id=GITHUB_CLIENT_ID, 
     client_secret=GITHUB_CLIENT_SECRET, 
