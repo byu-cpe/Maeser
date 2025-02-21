75995dba2b1944d0b1762049ee4c39e5c107d739
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-02-21 11:13:42 -0700

    Fixed location of App_Manager docstring to be picked up by Sphinx-Docs, and added a reference to App_Manager documentation to the Flask Example sphinx doc. Also fixed a few minor typos.

--- maeser/blueprints.py
+++ maeser/blueprints.py
@@ -50,68 +50,70 @@ from .controllers import (
     user_management_api,
 )
 
-class App_Manager:
 
-    def __init__(self, 
-        #app and app name
+class App_Manager:
+    """
+    Manages the Maeser App and its configurations.
+
+    Args:
+        app (Flask): The Flask application instance.
+        app_name (str | None, optional): The name of the application.
+        flask_secret_key (str): The secret key for the Flask application.
+        chat_session_manager (ChatSessionManager): The chat session manager instance.
+        user_manager (UserManager | None, optional): The user manager instance. Defaults to None.
+        main_logo_login (str | None, optional): URL or path to the login logo. Defaults to None.
+        main_logo_chat (str | None, optional): URL or path to the chat logo. Defaults to None.
+        chat_head (str | None, optional): URL or path to the chat header image. Defaults to None.
+        favicon (str | None, optional): URL or path to the favicon. Defaults to None.
+        login_text (str | None, optional): Text to display on the login page. Defaults to None.
+        changelog (str | None, optional): Text to display as the changelog. Defaults to None.
+        chat_greeting (str, optional): Greeting message to display in the chat. Defaults to "Hello, how can I help you today?".
+        branch_response (str, optional): Response message to display when a branch is selected. Defaults to "Okay, I'll help you with ${action}!".
+        animation (bool, optional): Whether to enable toggle animations. Defaults to False.
+        primary_color (str, optional): Primary color of the application. Defaults to "#333".
+        secondary_color (str, optional): Secondary color of the application. Defaults to "#ccc".
+        button_color (str, optional): Color of the buttons. Defaults to "#0084ff".
+        button_color_active (str, optional): Color of the active buttons. Defaults to "#009e15".
+        button_color_inactive (str, optional): Color of the inactive buttons. Defaults to "#ddd".
+        fafa_button (str, optional): Color of the fafa buttons. Defaults to "#eee".
+        logout_button (str, optional): Color of the logout button. Defaults to "#333".
+        new_chat_button (str, optional): Color of the new chat button. Defaults to "#333".
+        help_train_button (str, optional): Color of the help train button. Defaults to "#eee".
+    """
+
+    def __init__(
+        self,
+        # app and app name
         app: Flask,
         app_name: str,
-        #flask secret
+        # flask secret
         flask_secret_key: str,
-        #managers
-        chat_session_manager: ChatSessionManager, 
+        # managers
+        chat_session_manager: ChatSessionManager,
         user_manager: UserManager | None = None,
-        #images
+        # images
         main_logo_login: str | None = None,
         main_logo_chat: str | None = None,
         chat_head: str | None = None,
         favicon: str | None = None,
-        #text
+        # text
         login_text: str | None = None,
         changelog: str | None = None,
         chat_greeting: str = "Hello, how can I help you today?",
         branch_response: str = "Okay, I'll help you with ${action}!",
-        #toggle animations
+        # toggle animations
         animation: bool = False,
-        #colors
+        # colors
         primary_color: str = "#f5f5f5",
         secondary_color: str = "#ccc",
         button_color: str = "#0084ff",
-        button_color_active: str = "#009e15", 
-        button_color_inactive: str = "#e9e9e9", 
+        button_color_active: str = "#009e15",
+        button_color_inactive: str = "#e9e9e9",
         fafa_button: str = "#b8b8b8",
         logout_button: str = "#000000",
         new_chat_button: str = "#000000",
         help_train_button: str = "#a8a8a8",
     ):
-        """
-        Initialize the App Manager
-
-        Args:
-            app (Flask): The Flask application instance.
-            app_name (str | None, optional): The name of the application.
-            flask_secret_key (str): The secret key for the Flask application.
-            chat_session_manager (ChatSessionManager): The chat session manager instance.
-            user_manager (UserManager | None, optional): The user manager instance. Defaults to None.
-            main_logo_login (str | None, optional): URL or path to the login logo. Defaults to None.
-            main_logo_chat (str | None, optional): URL or path to the chat logo. Defaults to None.
-            chat_head (str | None, optional): URL or path to the chat header image. Defaults to None.
-            favicon (str | None, optional): URL or path to the favicon. Defaults to None.
-            login_text (str | None, optional): Text to display on the login page. Defaults to None.
-            changelog (str | None, optional): Text to display as the changelog. Defaults to None.
-            chat_greeting (str, optional): Greeting message to display in the chat. Defaults to "Hello, how can I help you today?".
-            branch_response (str, optional): Response message to display when a branch is selected. Defaults to "Okay, I'll help you with ${action}!".
-            animation (bool, optional): Whether to enable toggle animations. Defaults to False.
-            primary_color (str, optional): Primary color of the application. Defaults to "#333".
-            secondary_color (str, optional): Secondary color of the application. Defaults to "#ccc".
-            button_color (str, optional): Color of the buttons. Defaults to "#0084ff".
-            button_color_active (str, optional): Color of the active buttons. Defaults to "#009e15".
-            button_color_inactive (str, optional): Color of the inactive buttons. Defaults to "#ddd".
-            fafa_button (str, optional): Color of the fafa buttons. Defaults to "#eee".
-            logout_button (str, optional): Color of the logout button. Defaults to "#333".
-            new_chat_button (str, optional): Color of the new chat button. Defaults to "#333".
-            help_train_button (str, optional): Color of the help train button. Defaults to "#eee".
-        """
         self.app = app
         self.app_name = app_name
 
@@ -142,31 +144,31 @@ class App_Manager:
         self.new_chat_button = new_chat_button
         self.help_train_button = help_train_button
 
-        self.current_dir = os.path.dirname(os.path.abspath(__file__ + '/.'))
+        self.current_dir = os.path.dirname(os.path.abspath(__file__ + "/."))
 
         # The following functions with no code are work in progress and will be added soon
 
-    def template_styles_css (self):
+    def template_styles_css(self):
         """
         Template the styles.css file using Jinja2 templating
         """
 
-    def template_chat_interface (self):
+    def template_chat_interface(self):
         """
         Template the chat_interface.html file using Jinja2 templating
         """
 
-    def template_login (self):
+    def template_login(self):
         """
         Template the login.html file using Jinja2 templating
         """
 
-    def template_training (self):
+    def template_training(self):
         """
         Template the training.html file using Jinja2 templating
         """
 
-    def template_feedback (self):
+    def template_feedback(self):
         """
         Template the feedback_form.html file using Jinja2 templating
         """
@@ -179,19 +181,20 @@ class App_Manager:
             Flask: The Flask application instance with the blueprint registered.
         """
         maeser_blueprint = Blueprint(
-            'maeser',
+            "maeser",
             __name__,
-            template_folder=os.path.join(self.current_dir, 'data/templates'),
-            static_folder=os.path.join(self.current_dir, 'data/static'),
-            static_url_path='/maeser/static',
+            template_folder=os.path.join(self.current_dir, "data/templates"),
+            static_folder=os.path.join(self.current_dir, "data/static"),
+            static_url_path="/maeser/static",
         )
 
-        @maeser_blueprint.app_template_filter('datetimeformat')
-        def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
+        @maeser_blueprint.app_template_filter("datetimeformat")
+        def datetimeformat(value, format="%Y-%m-%d %H:%M:%S"):
             """Format a timestamp as a datetime string."""
             return datetime.fromtimestamp(value).strftime(format)
 
         if self.user_manager:
+
             def refresh_requests():
                 """Refresh user requests at regular intervals."""
                 while True:
@@ -203,32 +206,32 @@ class App_Manager:
             self.app.secret_key = self.flask_secret_key
             login_manager = LoginManager(self.app)
             login_manager.init_app(self.app)
-            login_manager.login_view = 'maeser.login'  # type: ignore
-            login_manager.session_protection = 'strong'
-            
+            login_manager.login_view = "maeser.login"  # type: ignore
+            login_manager.session_protection = "strong"
+
             @login_manager.user_loader
             def load_user(user_full_id: str):
                 """Load a user by their ID."""
-                auth_method, user_id = user_full_id.split('.', 1)
+                auth_method, user_id = user_full_id.split(".", 1)
                 return self.user_manager.get_user(auth_method, user_id)
 
-            @maeser_blueprint.route('/')
+            @maeser_blueprint.route("/")
             @login_required
             def chat_interface_route():
                 """Route for the chat interface."""
                 return chat_interface.controller(
-                    self.chat_session_manager, 
-                    self.user_manager.max_requests, 
-                    self.user_manager.rate_limit_interval, 
-                    current_user, 
+                    self.chat_session_manager,
+                    self.user_manager.max_requests,
+                    self.user_manager.rate_limit_interval,
+                    current_user,
                     main_logo_login=self.main_logo_login,
                     main_logo_chat=self.main_logo_chat,
                     favicon=self.favicon,
                     chat_head=self.chat_head,
-                    app_name=self.app_name
+                    app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/login', methods=['GET', 'POST'])
+            @maeser_blueprint.route("/login", methods=["GET", "POST"])
             def login():
                 """Route for the login interface."""
                 return login_api.login_controller(
@@ -236,82 +239,84 @@ class App_Manager:
                     main_logo_login=self.main_logo_login,
                     main_logo_chat=self.main_logo_chat,
                     favicon=self.favicon,
-                    app_name=self.app_name
+                    app_name=self.app_name,
                 )
-            
-            @maeser_blueprint.route('/login/github', methods=['GET'])
+
+            @maeser_blueprint.route("/login/github", methods=["GET"])
             def github_authorize():
                 """Route for GitHub authorization."""
                 return login_api.github_authorize_controller(
-                    current_user, 
-                    self.user_manager.authenticators['github']
+                    current_user, self.user_manager.authenticators["github"]
                 )
 
-            @maeser_blueprint.route('/login/github_callback')
+            @maeser_blueprint.route("/login/github_callback")
             def github_auth_callback():
                 """Route for GitHub authorization callback."""
                 return login_api.github_auth_callback_controller(
-                    current_user, 
+                    current_user,
                     self.user_manager,
                     main_logo_login=self.main_logo_login,
                     main_logo_chat=self.main_logo_chat,
                     favicon=self.favicon,
-                    app_name=self.app_name
+                    app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/logout')
+            @maeser_blueprint.route("/logout")
             @login_required
             def logout_route():
                 """Route for logging out."""
                 return logout.controller()
-            
-            @maeser_blueprint.route('/users')
+
+            @maeser_blueprint.route("/users")
             @login_required
             @admin_required(current_user)
             def manage_users():
                 """Route for managing users."""
                 return manage_users_view.controller(
-                    self.user_manager, 
+                    self.user_manager,
                     main_logo_login=self.main_logo_login,
-                    main_logo_chat=self.main_logo_chat, 
-                    favicon=self.favicon, 
-                    app_name=self.app_name
+                    main_logo_chat=self.main_logo_chat,
+                    favicon=self.favicon,
+                    app_name=self.app_name,
                 )
-            
-            @maeser_blueprint.route('/users/api', methods=['POST'])
+
+            @maeser_blueprint.route("/users/api", methods=["POST"])
             @login_required
             @admin_required(current_user)
             def manage_users_api():
                 """API route for managing users."""
                 return user_management_api.controller(self.user_manager)
 
-            @maeser_blueprint.route('/req_session', methods=['POST'])
+            @maeser_blueprint.route("/req_session", methods=["POST"])
             @login_required
             def sess_handler():
                 """Route for handling session requests."""
                 return new_session_api.controller(self.chat_session_manager, True)
-            
-            @maeser_blueprint.route('/msg/<chat_session>', methods=['POST'])
+
+            @maeser_blueprint.route("/msg/<chat_session>", methods=["POST"])
             @login_required
             @rate_limited(self.user_manager, current_user)
             def msg_api(chat_session):
                 """API route for handling chat messages."""
                 return chat_api.controller(self.chat_session_manager, chat_session)
-            
-            @maeser_blueprint.route('/feedback', methods=['POST'])
+
+            @maeser_blueprint.route("/feedback", methods=["POST"])
             @login_required
             def feedback():
                 """Route for submitting feedback."""
                 return feedback_api.controller(self.chat_session_manager)
-            
-            @self.app.route('/get_requests_remaining', methods=['GET'])
+
+            @self.app.route("/get_requests_remaining", methods=["GET"])
             @login_required
             def get_requests_remaining():
                 """Route for getting the remaining requests."""
-                return remaining_requests_api.controller(self.user_manager, current_user)
+                return remaining_requests_api.controller(
+                    self.user_manager, current_user
+                )
 
         else:
-            @maeser_blueprint.route('/')
+
+            @maeser_blueprint.route("/")
             def chat_interface_route():
                 """Route for the chat interface."""
                 return chat_interface.controller(
@@ -320,26 +325,27 @@ class App_Manager:
                     main_logo_chat=self.main_logo_chat,
                     favicon=self.favicon,
                     chat_head=self.chat_head,
-                    app_name=self.app_name
+                    app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/req_session', methods=['POST'])
+            @maeser_blueprint.route("/req_session", methods=["POST"])
             def sess_handler():
                 """Route for handling session requests."""
                 return new_session_api.controller(self.chat_session_manager)
-            
-            @maeser_blueprint.route('/msg/<chat_session>', methods=['POST'])
+
+            @maeser_blueprint.route("/msg/<chat_session>", methods=["POST"])
             def msg_api(chat_session):
                 """API route for handling chat messages."""
                 return chat_api.controller(self.chat_session_manager, chat_session)
-            
-            @maeser_blueprint.route('/feedback', methods=['POST'])
+
+            @maeser_blueprint.route("/feedback", methods=["POST"])
             def feedback():
                 """Route for submitting feedback."""
                 return feedback_api.controller(self.chat_session_manager)
 
         if self.chat_session_manager.chat_logs_manager:
-            @maeser_blueprint.route('/train')
+
+            @maeser_blueprint.route("/train")
             @login_required if self.user_manager else lambda x: x
             def train():
                 """Route for training."""
@@ -350,13 +356,13 @@ class App_Manager:
                     app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/submit_train', methods=['POST'])
+            @maeser_blueprint.route("/submit_train", methods=["POST"])
             @login_required if self.user_manager else lambda x: x
             def submit_train():
                 """Route for submitting training data."""
                 return training_post.controller(self.chat_session_manager)
 
-            @maeser_blueprint.route('/feedback_form')
+            @maeser_blueprint.route("/feedback_form")
             def feedback_form():
                 """Route for getting the feedback form."""
                 return feedback_form_get.controller(
@@ -366,12 +372,12 @@ class App_Manager:
                     app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/submit_feedback', methods=['POST'])
+            @maeser_blueprint.route("/submit_feedback", methods=["POST"])
             def submit_feedback():
                 """Route for submitting feedback."""
                 return feedback_form_post.controller(self.chat_session_manager)
-            
-            @maeser_blueprint.route('/logs', methods=['GET'])
+
+            @maeser_blueprint.route("/logs", methods=["GET"])
             @login_required if self.user_manager else lambda x: x
             @admin_required(current_user) if self.user_manager else lambda x: x
             def logs():
@@ -379,10 +385,10 @@ class App_Manager:
                 return chat_logs_overview.controller(
                     self.chat_session_manager,
                     favicon=self.favicon,
-                    app_name=self.app_name
+                    app_name=self.app_name,
                 )
 
-            @maeser_blueprint.route('/logs/<branch>/<filename>')
+            @maeser_blueprint.route("/logs/<branch>/<filename>")
             @login_required if self.user_manager else lambda x: x
             @admin_required(current_user) if self.user_manager else lambda x: x
             def display_log(branch, filename):
@@ -391,7 +397,7 @@ class App_Manager:
                     self.chat_session_manager, branch, filename, app_name=self.app_name
                 )
 
-            @maeser_blueprint.route('/conversation_history', methods=['POST'])
+            @maeser_blueprint.route("/conversation_history", methods=["POST"])
             @login_required if self.user_manager else lambda x: x
             def conversation_history():
                 """Route for getting conversation history."""
