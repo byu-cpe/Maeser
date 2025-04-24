6d3cfd91ccd968f003675c413f7bf1848a2b929b
Gohaun Manley <gohaun@byu.edu>
2025-04-24 12:39:20 -0600

    found error

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -72,7 +72,7 @@ release = 'alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinxcontrib.mermaid']
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinxcontrib-mermaid']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
