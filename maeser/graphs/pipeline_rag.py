b1d0816fe219a550148e2aa757606c9295d59f21
adamsandland <adam@sandlands.org>
2025-07-09 16:10:57 -0600

    fixed missing vectorstores

--- maeser/graphs/pipeline_rag.py
+++ maeser/graphs/pipeline_rag.py
@@ -18,7 +18,7 @@ Maeser. If not, see <https://www.gnu.org/licenses/>.
 """
 from langchain_core.documents.base import Document
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langgraph.graph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph, START, END
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage
