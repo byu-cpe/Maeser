c4a6a8f57c5ec1506891e71c9968fac0b453f52d
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-16 09:23:06 -0600

    remove copyright disclaimers from python scripts

--- maeser/render.py
+++ maeser/render.py
@@ -4,21 +4,6 @@ Markdown response conversion module. Intended for use with LLM output.
 This module provides a utility function to easily customize HTML layouts and convert markdown 
 responses to HTML with additional processing, such as adding target="_blank" to anchor tags 
 and adjusting paths for images.
-
-© 2024
-
-This file is part of Maeser.
-
-Maeser is free software: you can redistribute it and/or modify it under the terms of
-the GNU Lesser General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
-
-Maeser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along with
-Maeser. If not, see <https://www.gnu.org/licenses/>.
 """
 
 import markdown
