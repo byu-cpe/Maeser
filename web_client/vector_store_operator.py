930ee72ad698eea170ec816e17a7d3f6c7229efc
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-18 19:45:29 -0600

    Add source name to metadata when vectorizing pdfs

--- web_client/vector_store_operator.py
+++ web_client/vector_store_operator.py
@@ -25,16 +25,23 @@ def vectorize_data(output_dir: str):
     Args:
         output_dir (str): The directory where the text files are located and where the vectorstore will be saved.
     """
-    texts = []
 
+    # Read in all texts
+    texts = []
+    metadatas = []
     for filename in os.listdir(output_dir):
         if filename.endswith(".md"):
+            source_name = os.path.splitext(filename)[0].replace("_", " ")
             with open(os.path.join(output_dir, filename), "r", encoding="utf-8") as f:
                 texts.append(f.read())
+                metadatas.append({"source": source_name})
 
     # Split all loaded texts into documents
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    documents = text_splitter.create_documents(texts)
+    documents = text_splitter.create_documents(
+        texts=texts,
+        metadatas=metadatas,
+    )
 
     # Save the vectorized text to a local FAISS vectorstore
     db = FAISS.from_documents(documents, OpenAIEmbeddings())
