93a76ad98073def2d1589b08770a0b607829b008
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-18 18:55:25 -0600

    upgrade pdf conversion to convert to llm friendly markdown format

--- Makefile
+++ Makefile
@@ -16,6 +16,7 @@ setup: $(VENV)/bin/activate
 	$(PIP) install tiktoken
 	$(PIP) install PyMuPDF
 	$(PIP) install pymupdf-stubs
+	$(PIP) install pymupdf4llm
 	$(PIP) install Pillow
 	$(PIP) install discord
 	@echo "Updating poetry lock file if necessary..."
--- web_client/design_model.py
+++ web_client/design_model.py
@@ -6,8 +6,10 @@ from flask import request
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 import os
+import pathlib
 import subprocess
 import shutil
+import pymupdf4llm
 from rename_files import rename_pdfs
 from extract_figures import extract_all_figures
 from vector_store_operator import vectorize_data
@@ -119,15 +121,12 @@ def process_datasets(model_dir: str):
         print("1. Renaming PDF files...")
         rename_pdfs(dir)
 
-        print("2. Converting PDFs to text...")
+        print("2. Converting PDFs to markdown (this may take a moment)...")
         pdf_files = sorted([f for f in os.listdir(dir) if f.lower().endswith(".pdf")])
         for pdf in pdf_files:
             filename = os.path.splitext(pdf)[0]+".txt"
-            # TODO: Replace pdftotext with a PyMuPDF
-            subprocess.run(
-                ["pdftotext", os.path.join(dir, pdf), os.path.join(dir, filename)],
-                check=True,
-            )
+            md_text = pymupdf4llm.to_markdown(os.path.join(dir, pdf))
+            pathlib.Path(os.path.join(dir, filename)).write_bytes(md_text.encode())
 
         print("3. Extracting figures from PDFs...")
         extract_all_figures(dir)
@@ -208,9 +207,9 @@ def load_datasets(model_dir: str) -> list[str]:
     Returns:
         list[str]: The names of the model's datasets.
     """
-    datasets = [
+    datasets = sorted([
         d for d in os.listdir(model_dir)
         if os.path.isdir(os.path.join(model_dir, d)) and d != '__pycache__'
-    ]
+    ])
 
     return datasets
