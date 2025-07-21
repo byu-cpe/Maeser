6d07c0fc6d41b5480492fde11532ca6d2e1902f9
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-21 15:37:29 -0600

    change config_paths in example/config_example.py to look for config.yaml instead of config_example.yaml

--- example/config_example.py
+++ example/config_example.py
@@ -6,9 +6,9 @@ import os
 def load_config():
     """Load configuration from YAML file."""
     config_paths = [
-        'config_example.yaml',
-        './config_example.yaml',
-        'example/config_example.yaml'
+        'config.yaml',
+        './config.yaml',
+        'example/config.yaml',
     ]
     
     for path in config_paths:
