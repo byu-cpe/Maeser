962ca5faa2cc638855104287913601c940f6f23c
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-23 10:16:25 -0600

    Rename App_Manager to AppManager and update example flask scripts accordingly

--- example/flask_multigroup_user_mangement_example.py
+++ example/flask_multigroup_user_mangement_example.py
@@ -76,11 +76,11 @@ from flask import Flask
 
 base_app = Flask(__name__)
 
-from maeser.blueprints import App_Manager
+from maeser.blueprints import AppManager
 
 # Create the App_Manager class
 
-app_manager = App_Manager(
+app_manager = AppManager(
     app=base_app,
     app_name="Maeser Test App",
     flask_secret_key="secret",
