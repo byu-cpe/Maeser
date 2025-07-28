dbd13b6790d26594db4371f09d8a6e8f61a9b0aa
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-07-28 17:01:20 -0600

    add sphinx-copybutton to doc extensions

--- sphinx-docs/conf.py
+++ sphinx-docs/conf.py
@@ -60,7 +60,14 @@ release = 'alpha'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinxcontrib.mermaid']
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx_copybutton',
+    'sphinxcontrib.mermaid',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
