e221a2862f19a7b43196910204a672070a49045f
ayden <parfriskus@gmail.com>
2025-08-07 15:03:28 -0600

    update source documentation for maeser.controllers.conversation_history_api

--- maeser/controllers/conversation_history_api.py
+++ maeser/controllers/conversation_history_api.py
@@ -9,25 +9,25 @@ system messages by applying HTML response formatting.
 """
 
 from flask import jsonify, request
+from maeser.chat.chat_session_manager import ChatSessionManager
 from maeser.render import get_response_html
 
-def controller(session_handler):
+def controller(session_handler: ChatSessionManager):
     """
     Retrieves the conversation history for a given session and branch.
 
+    The function uses flask.request and expects a post request that contains 'session' and 'branch' keys,
+    which are used to retrieve the conversation history from **session_handler**.
+    If the conversation history contains 'messages', it iterates through them and processes
+    the content of system messages using the `get_response_html` function. Finally, it
+    returns the conversation history as a JSON response.
+
     Args:
-        session_handler (object): An object that handles session management
-            and provides a method to retrieve conversation history.
+        session_handler (ChatSessionManager): The chat session manager for the Maeser application.
 
     Returns:
         dict: A dictionary containing the conversation history, with system
             messages having their content processed by the `get_response_html` function.
-
-    The function expects the request data to contain 'session' and 'branch' keys,
-    which are used to retrieve the conversation history from the `session_handler` object.
-    If the conversation history contains 'messages', it iterates through them and processes
-    the content of system messages using the `get_response_html` function. Finally, it
-    returns the conversation history as a JSON response.
     """
     data = request.get_json()
 
