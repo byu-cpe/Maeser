adb97ed9e8825d96fc66c718172bc09691d44d62
ayden <parfriskus@gmail.com>
2025-08-07 15:58:12 -0600

    update source code documentation for maeser.controllers.feedback_form_get

--- maeser/controllers/feedback_form_get.py
+++ maeser/controllers/feedback_form_get.py
@@ -8,12 +8,17 @@ This module contains the controller function to render the feedback form templat
 from flask import render_template
 
 def controller(app_name: str | None = None, main_logo_chat: str | None = None, favicon: str | None = None) -> str:
-    """
-    Display the feedback form.
+    """ Display the feedback form.
+
+    Args:
+        app_name (str | None): The display name of the Maeser application. Defaults to None.
+        main_logo_chat (str | None): The logo to display in the page header. Defaults to None.
+        favicon (str | None): The favicon for the page. Defaults to None.
 
     Returns:
         str: Rendered feedback form template.
-    """
+    """    
+
     role_options = ['Undergraduate Student', 'Graduate Student', 'Faculty', 'Other']
     category_options = ['Other', 'General Feedback', 'Bug Report', 'Feature Request', 'Content Issue']
     return render_template(
