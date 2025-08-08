efd9f980085074cc4aaece56c82b1b2e718fdcd4
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-08-08 11:50:22 -0600

    update source documentation for maeser.controllers.training_post

--- maeser/controllers/training_post.py
+++ maeser/controllers/training_post.py
@@ -1,19 +1,29 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 """
-This module contains functions to save training data and handle controller
-requests in a Flask application.
+This module contains the controller to save training data from a post request.
 """
 
 from maeser.chat.chat_session_manager import ChatSessionManager
-from flask import request, redirect
+from flask import request, redirect, Response
 
-def controller(chat_session_manager: ChatSessionManager):
+def controller(chat_session_manager: ChatSessionManager) -> Response:
     """
-    Handle the POST request to save training data.
+    Handle the post request to save training data.
+
+    Uses flask.request and expects a post request with the following fields:
+
+    - "**name**": The name of the form respondent.
+    - "**role**": The role/position of the respondent. Expected values are "Professor" or "Teachers Assistant".
+    - "**type**": The type of training data. Expected values are "Information" or "Style".
+    - "**question**": The question the user would ask the chatbot.
+    - "**answer**": The answer the chatbot should generate in response to the user's question.
 
     Args:
         chat_session_manager (ChatSessionManager): The chat session manager instance.
+
+    Returns:
+        Response: Redirects to the home page.
     """
     chat_logs_manager = chat_session_manager.chat_logs_manager
     
