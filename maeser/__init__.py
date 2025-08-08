9989ec59b89f9e8164b0675a0246d0d30c13464c
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-08-08 15:41:09 -0600

    Add maeser.graphs to top level package __init__.py

--- maeser/__init__.py
+++ maeser/__init__.py
@@ -6,19 +6,25 @@ functions for managing a chat application.
 
 The package is organized as follows:
 
-- `chat`: This module contains classes and functions related to chat functionality,
-          such as sending and receiving messages.
-- `controllers`: This module contains classes and functions for managing Flask
-          controllers, which handle the logic for handling incoming requests.
-- `user_manager`: This module contains classes and functions for managing users
-          in the chat application.
-- `render`: This module contains classes and functions for rendering the user
-          interface of the chat application.
+- **chat**: This module contains classes and functions related to chat functionality, such as sending and receiving messages.
+- **controllers**: This module contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
+- **graphs**: This package contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
+- **user_manager**: This module contains classes and functions for managing users in the chat application.
+- **render**: This module contains classes and functions for rendering the user interface of the chat application.
 """
 
-from . import chat
-from . import controllers
-from . import user_manager
-from . import render
+from . import (
+    chat,
+    controllers,
+    graphs,
+    user_manager,
+    render,
+)
 
-__all__ = ['chat', 'controllers', 'user_manager', 'render']
+__all__ = [
+    'chat',
+    'controllers',
+    'graphs',
+    'user_manager',
+    'render'
+]
