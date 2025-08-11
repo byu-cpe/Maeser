c3d0fcfef36a8e52fa7877992fe0ad99e0c3a622
ayden <parfriskus@gmail.com>
2025-08-11 10:38:15 -0600

    Add blueprints module to top level __init__.py

--- maeser/__init__.py
+++ maeser/__init__.py
@@ -9,6 +9,7 @@ The package is organized as follows:
 - **chat**: This module contains classes and functions related to chat functionality, such as sending and receiving messages.
 - **controllers**: This module contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
 - **graphs**: This package contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
+- **blueprints**: This module sets up the Flask blueprint and associated routes for the Maeser application.
 - **user_manager**: This module contains classes and functions for managing users in the chat application.
 - **render**: This module contains classes and functions for rendering the user interface of the chat application.
 """
@@ -17,6 +18,7 @@ from . import (
     chat,
     controllers,
     graphs,
+    blueprints,
     user_manager,
     render,
 )
@@ -25,6 +27,7 @@ __all__ = [
     'chat',
     'controllers',
     'graphs',
+    'blueprints',
     'user_manager',
     'render'
 ]
