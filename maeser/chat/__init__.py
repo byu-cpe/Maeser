6b367266e209000f1ca9e329fa30baeea6999dc1
ayden <parfriskus@gmail.com>
2025-08-11 10:42:33 -0600

    Format fields for sublevel __init__.py files

--- maeser/chat/__init__.py
+++ maeser/chat/__init__.py
@@ -5,8 +5,8 @@ This is the chat subpackage for the Maeser package.
 
 This package contains the following subpackages and modules:
 
-- `chat_logs`: This module provides functionality for managing chat logs.
-- `chat_session_manager`: This module provides functionality for managing chat sessions.
+:chat_logs: This module provides functionality for managing chat logs.
+:chat_session_manager: This module provides functionality for managing chat sessions.
 """
 
 from maeser._utils.pkg_utils import autoimport_all
--- maeser/graphs/__init__.py
+++ maeser/graphs/__init__.py
@@ -6,9 +6,9 @@ This is the graphs subpackage for the Maeser package.
 This package contains **Retrieval-Augmented Generation (RAG) graphs** that affect the workflow and behavior of the chatbot.
 Maeser's RAG graphs and their functionality are as follows:
 
-- **simple_rag**: Accepts only one vector store, forcing the chatbot to stick to one topic per conversation.
-- **pipeline_rag**: Accepts multiple vector stores, allowing the chatbot to dynamically choose the most relevant vector store when answering a user's question. However, only one vector store can be accessed per response.
-- **universal_rag**: Accepts multiple vector stores, like **pipeline_rag**, but also pulls from as many vector stores as needed (or none at all) per response.
+:simple_rag: Accepts only one vector store, forcing the chatbot to stick to one topic per conversation.
+:pipeline_rag: Accepts multiple vector stores, allowing the chatbot to dynamically choose the most relevant vector store when answering a user's question. However, only one vector store can be accessed per response.
+:universal_rag: Accepts multiple vector stores, like **pipeline_rag**, but also pulls from as many vector stores as needed (or none at all) per response.
 """
 
 from maeser._utils.pkg_utils import autoimport_all
