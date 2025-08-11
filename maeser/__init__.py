84dd7bdb4461d390a4fbe2f6cced7aa756377998
ayden <parfriskus@gmail.com>
2025-08-11 10:40:18 -0600

    Format package fields in top level __init__.py

--- maeser/__init__.py
+++ maeser/__init__.py
@@ -6,12 +6,12 @@ functions for managing a chat application.
 
 The package is organized as follows:
 
-- **chat**: This module contains classes and functions related to chat functionality, such as sending and receiving messages.
-- **controllers**: This module contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
-- **graphs**: This package contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
-- **blueprints**: This module sets up the Flask blueprint and associated routes for the Maeser application.
-- **user_manager**: This module contains classes and functions for managing users in the chat application.
-- **render**: This module contains classes and functions for rendering the user interface of the chat application.
+:maeser.chat: This module contains classes and functions related to chat functionality, such as sending and receiving messages.
+:maeser.controllers: This module contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
+:maeser.graphs: This package contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
+:maeser.blueprints: This module sets up the Flask blueprint and associated routes for the Maeser application.
+:maeser.user_manager: This module contains classes and functions for managing users in the chat application.
+:maeser.render: This module contains classes and functions for rendering the user interface of the chat application.
 """
 
 from . import (
