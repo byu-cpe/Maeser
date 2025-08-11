0887f1138b4b688ef233dc3702f94c4aadd6e6f6
ayden <parfriskus@gmail.com>
2025-08-11 13:27:32 -0600

    Fix capitalization for vector store

--- example/apps/universal/terminal_universal.py
+++ example/apps/universal/terminal_universal.py
@@ -28,8 +28,8 @@ from langgraph.graph.graph import CompiledGraph
 # One for the history of BYU and one for the life of Karl G. Maeser.
 # Ensure that topics are all lower case and spaces between words
 vectorstore_config = {
-    "byu history": f"{VEC_STORE_PATH}/byu",      # Vector Store for BYU history.
-    "karl g maeser": f"{VEC_STORE_PATH}/maeser"  # Vector Store for Karl G. Maeser.
+    "byu history": f"{VEC_STORE_PATH}/byu",      # Vector store for BYU history.
+    "karl g maeser": f"{VEC_STORE_PATH}/maeser"  # Vector store for Karl G. Maeser.
 }
 
 byu_maeser_universal_rag: CompiledGraph = get_universal_rag(
