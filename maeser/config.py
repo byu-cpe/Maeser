82c852e0e7564593a90dd2d865c260827994ace4
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-08-18 13:39:58 -0600

    List all attributes of maeser.config module in source docstring

--- maeser/config.py
+++ maeser/config.py
@@ -1,16 +1,58 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
 """
-This module is used to locate `config.yaml` and expose its values for use by the example Maeser dynamic implementations/handlers.
-**config_paths** determines where this module will look for `config.yaml`.
+This module is used to locate `config.yaml` and expose its values for use by other modules in the Maeser package.
+**config_paths** determines where this module will look for `config.yaml`. Currently, only the working directory is searched.
+
+Attributes:
+    OPENAI_API_KEY (str): The API key for an OpenAI LLM.
+    GITHUB_CLIENT_SECRET (str): The secret key for a GitHub OAuth2 application.
+
+    COURSE_ID (str): The course ID the module importing this config should use.
+
+    GITHUB_CLIENT_ID (str): The client ID for a GitHub OAuth2 application.
+        This should be the same application referenced in OPENAI_API_KEY
+    GITHUB_AUTH_CALLBACK_URI (str): the callback route/URL that the user should be sent when GitHub Authentication is complete.
+        This should follow the format "http://www.example.com/login/github_callback".
+    GITHUB_TIMEOUT (int): The amount of time before a GitHub Authentication attempt should time out.
+
+    LDAP3_NAME (str): The name of the LDAP server.
+    LDAP_SERVER_URLS (list[str]): The URLs associated with the LDAP server.
+    LDAP_BASE_DN (str): The Base Distinguished Name of the LDAP server.
+    LDAP_ATTRIBUTE_NAME (str): The attribute name of the LDAP server.
+    LDAP_SEARCH_FILTER (str): The search filter of the LDAP server.
+    LDAP_OBJECT_CLASS (str): The object class of the LDAP server.
+    LDAP_ATTRIBUTES (list): The attributes of the LDAP server.
+    LDAP_CA_CERT_PATH (str): The path to the LDAP server's CA certification.
+    LDAP_CONNECTION_TIMEOUT (int): The amount of time before an LDAP Authentication attempt should time out.
+
+    MAX_REQUESTS (int): The maximum number of requests a user can send before being rate-limited.
+    RATE_LIMIT_INTERVAL (int): The interval in which requests are granted to a user.
+    LOG_SOURCE_PATH (str): The path to the chat logs directory.
+
+    VEC_STORE_PATH (str): The path to the courses/vector stores.
+    VEC_STORE_TYPE (str): The type of vector store.
+
+    LLM_MODEL_NAME (str): The name of the LLM.
+    LLM_PROVIDER (str): The provider of the LLM.
+    LLM_TOKEN_LIMIT (int): The max number of tokens the LLM should process.
+
+    EMBED_MODEL (str): The model used to embed user inputs.
+    EMBED_PROVIDER (str): The provider of the embeddings model.
+
+    USERS_DB_PATH (str): The path to the file used to manage the user database.
+    CHAT_HISTORY_PATH (str): The path to the chat history logs.
+
+    DISCORD_BOT_TOKEN (str): The token for a Discord bot.
+    DISCORD_INTRO (str): An intro message that the Discord bot should say when prompted.
 """
 
 import yaml
 import os
 
 
-def load_config():
-    """Load configuration from YAML file."""
+def load_config() -> None:
+    """Searches the working directory for "config.yaml" and loads it via **yaml.safe_load()**."""
     config_paths = [
         "config.yaml",
         "./config.yaml",
@@ -44,7 +86,7 @@ GITHUB_TIMEOUT: int = config.get("github", {}).get("timeout", 10)
 
 # LDAP3 Auth
 LDAP3_NAME: str = config.get("ldap3", {}).get("name", "CAEDM")
-LDAP_SERVER_URLS: list = config.get("ldap3", {}).get("ldap_server_urls", [])
+LDAP_SERVER_URLS: list[str] = config.get("ldap3", {}).get("ldap_server_urls", [])
 LDAP_BASE_DN: str = config.get("ldap3", {}).get("ldap_base_dn")
 LDAP_ATTRIBUTE_NAME: str = config.get("ldap3", {}).get("attribute_name")
 LDAP_SEARCH_FILTER: str = config.get("ldap3", {}).get("search_filter")
@@ -82,7 +124,3 @@ CHAT_HISTORY_PATH: str = config.get("user_management", {}).get("chat_history_pat
 # Discord
 DISCORD_BOT_TOKEN: str = config.get("discord", {}).get("discord_token")
 DISCORD_INTRO: str = config.get("discord", {}).get("intro")
-
-# Teams Bot Credentials (Obtain from Azure Bot Service)
-TEAMS_APP_ID: str = os.getenv("TEAMS_APP_ID", "")
-TEAMS_APP_PASSWORD: str = os.getenv("TEAMS_APP_PASSWORD", "")
