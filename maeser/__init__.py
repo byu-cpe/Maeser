4786413d05b56150808af6987d3ede217e9ead7f
Parsistence <66070676+Parsistence@users.noreply.github.com>
2025-08-18 12:27:10 -0600

    Revise formatting in top-level __init__.py docstring

--- maeser/__init__.py
+++ maeser/__init__.py
@@ -6,12 +6,12 @@ functions for managing a chat application.
 
 The package is organized as follows:
 
-:maeser.chat: This module contains classes and functions related to chat functionality, such as sending and receiving messages.
-:maeser.controllers: This module contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
-:maeser.graphs: This package contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
+:maeser.chat: This subpackage contains classes and functions related to chat functionality, such as sending and receiving messages.
+:maeser.controllers: This subpackage contains classes and functions for managing Flask controllers, which handle the logic for handling incoming requests.
+:maeser.graphs: This subpackage contains Retrieval-Augmented Generation (RAG) graphs that affect the workflow and behavior of the chatbot.
 :maeser.blueprints: This module sets up the Flask blueprint and associated routes for the Maeser application.
-:maeser.user_manager: This module contains classes and functions for managing users in the chat application.
 :maeser.render: This module contains classes and functions for rendering the user interface of the chat application.
+:maeser.user_manager: This module contains classes and functions for managing users in the chat application.
 """
 
 from . import (
